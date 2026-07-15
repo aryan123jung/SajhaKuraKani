402da9f715a2683a940e9ae873a53630a7a44b32
aryan123jung <aryanjungchhetri@gmail.com>
2026-07-15 18:49:31 +0545

    added the path to app

--- sajhakurakani_backend/src/app.ts
+++ sajhakurakani_backend/src/app.ts
@@ -1,6 +1,8 @@
 import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import authRoutes from './routes/auth.routes';
+import { HttpError } from './errors/http-error';
 
 dotenv.config();
 
@@ -12,9 +14,14 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 app.use(express.json());
+app.use('/api/auth', authRoutes);
 
-app.get('/', (_req: Request, res: Response) => {
-  res.json({ message: 'SajhaKuraKani backend is running' });
+
+app.use((err: Error, req: Request, res: Response, next: Function) => {
+    if (err instanceof HttpError) {
+        return res.status(err.statusCode).json({ success: false, message: err.message });
+    }
+    return res.status(500).json({ success: false, message: err.message || "Internal Server Error" });
 });
 
 export default app;
