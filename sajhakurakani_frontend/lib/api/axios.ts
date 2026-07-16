5813b640da9255f78d8d1618ce10b7cf7eb69a4a
aryan123jung <aryanjungchhetri@gmail.com>
2026-07-16 19:02:53 +0545

    https added

--- sajhakurakani_frontend/lib/api/axios.ts
+++ sajhakurakani_frontend/lib/api/axios.ts
@@ -1,15 +1,24 @@
 import "server-only";
 
 import axios from "axios";
+import https from "https";
 import { getAuthToken } from "../cookie";
 
 const BASE_URL = process.env.NEXT_PUBLIC_API_URL;
+const useLocalHttpsAgent =
+  process.env.NODE_ENV !== "production" &&
+  typeof BASE_URL === "string" &&
+  BASE_URL.startsWith("https://localhost");
 
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
   headers: {
     "Content-Type": "application/json",
   },
+  // https implementation
+  httpsAgent: useLocalHttpsAgent
+    ? new https.Agent({ rejectUnauthorized: false })
+    : undefined,
 });
 
 axiosInstance.interceptors.request.use(
